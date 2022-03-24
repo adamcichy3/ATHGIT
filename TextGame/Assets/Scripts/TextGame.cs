13edbdcdc4b950823412f838200128add92a6c95
Adam Cichy <adam.cichyy3@gmail.com>
2022-03-24 18:20:04 +0100

    SuperHiperScript to debug jump Hello World

--- TextGame/Assets/Scripts/TextGame.cs
+++ TextGame/Assets/Scripts/TextGame.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class TextGame : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     void Start()
     {
+        Debug.Log("Hello World");
     }
 
-    // Update is called once per frame
     void Update()
     {
         
